831ddfa223e492e2264e12bb764fe3e66de20b9c
Laura Schulz <lrs8810@gmail.com>
2019-08-22 06:52:22 -0600

    Add unicorn test and implementation

---
+++ mythical-creatures/lib/unicorn.rb
@@ -0,0 +1,18 @@
+class Unicorn
+attr_reader :name
+attr_accessor :color
+
+  def initialize(name, color = "white")
+    @name = name
+    @color = color
+  end
+
+  def white?
+    @color == "white"
+  end
+
+  def say(phrase)
+    "**;* #{phrase} **;*"
+  end
+
+end
